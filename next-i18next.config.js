93f32d6e1e7ede06ca3c3e2eed830090beea1104
YuenSzeHong <szehong26@gmail.com>
2022-12-10 08:49:29 +0800

    added globalization support

---
+++ next-i18next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-i18next').UserConfig} */
+
+export const i18n = {
+    locales: ['en-US', 'zh-TW', 'zh-CN'],
+    defaultLocale: 'en-US',
+}
--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+import { i18n } from './next-i18next.config'
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  i18n
 }
-
 module.exports = nextConfig
