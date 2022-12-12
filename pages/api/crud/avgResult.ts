a914d5ecd4461459bc0eab33868b143ca5bf3e37
hghyv <chanipning@gmail.com>
2022-12-12 23:17:27 +0800

    crud

---
+++ pages/api/crud/avgResult.ts
@@ -0,0 +1,31 @@
+import { equal, strictEqual } from "assert";
+import { NextApiRequest, NextApiResponse } from "next";
+import db from "../../../lib/db";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    switch (req.method) {
+        case 'GET':
+            return getAvg();
+        default:
+            return res.status(405).end(`Method ${req.method} Not Allowed`)
+    }
+    async function getAvg(){
+    
+        const { module_id } = req.query;
+        
+        if(!module_id){
+            return res.status(400).json({ message: "module not found" });
+        }
+        else{
+            const records = await db.examResult
+            .select(["score", "module_id.id"]).filter("module_id.id", module_id as string)
+            .getAll();
+               
+        const sum = records.reduce((acc, cur) => acc + cur.score, 0);
+        const avg = sum / records.length;
+        }
+    }
+}
